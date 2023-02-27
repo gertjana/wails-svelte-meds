094da0cd10fd36815ed66958b7b7e0bdbe73877b
Gertjan Assies <gertjan.assies@gmail.com>
2023-02-27 21:25:09 +0100

    Asks to save before quiting

--- app.go
+++ app.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	_ "image/png"
+	"log"
 	"os"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 // App struct
@@ -15,6 +19,8 @@ var medications []Medication = make([]Medication, 0)
 
 var currentUser string
 
+var unsaved = false
+
 // NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
@@ -25,6 +31,36 @@ func NewApp() *App {
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 	a.Load()
+}
+
+func (a *App) beforeClose(ctx context.Context) (prevent bool) {
+	if !unsaved {
+		return false
+	}
+	dialog, err := runtime.MessageDialog(ctx, runtime.MessageDialogOptions{
+		Type:          runtime.QuestionDialog,
+		Title:         "Really want to quit?",
+		Message:       "what about those changes you made>",
+		Buttons:       []string{"Cancel", "Save", "Quit"},
+		DefaultButton: "Save",
+		CancelButton:  "Cancel",
+	})
+	if err != nil {
+		return false
+	}
+	switch dialog {
+	case "Quit":
+		return false
+	case "Save":
+		log.Println("Saving...")
+		a.Save(medications)
+		return false
+	}
+	return true
+}
+
+func (a *App) MarkUnsaved(u bool) {
+	unsaved = u
 }
 
 func getFileLocation(name string) string {
